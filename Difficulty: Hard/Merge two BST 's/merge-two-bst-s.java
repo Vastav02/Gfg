bd1146638ce3119096a1949719852871f0e32d00
Vastav02 <95334618+Vastav02@users.noreply.github.com>
2024-07-13 19:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge two BST 's/merge-two-bst-s.java
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+
+            s = br.readLine();
+            Node root2 = buildTree(s);
+
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1, root2);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        List<Integer>list1 = new ArrayList<>();
+        List<Integer>list2 = new ArrayList<>();
+        inorder(list1,root1);
+        inorder(list2,root2);
+        
+        /* merge two sorted list */
+        List<Integer>merge= new ArrayList<>();
+        int n = list1.size(),m = list2.size();
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(list1.get(i)<list2.get(j)){
+                merge.add(list1.get(i++));
+            }else{
+                merge.add(list2.get(j++));
+            }
+        }
+        while(i<n)merge.add(list1.get(i++));
+        while(j<m)merge.add(list2.get(j++));
+        return merge;
+    }
+    
+    public void inorder(List<Integer>list, Node root){
+        if(root == null)return;
+        inorder(list,root.left);
+        list.add(root.data);
+        inorder(list,root.right);
+    }
+}
+
