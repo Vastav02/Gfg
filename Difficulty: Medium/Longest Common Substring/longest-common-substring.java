29a4e1c8c76c0119f87979ec89021c0c9c494567
Vastav02 <95334618+Vastav02@users.noreply.github.com>
2024-08-14 19:56:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,37 +1,32 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            String input[] = read.readLine().trim().split(" ");
-            int n = Integer.parseInt(input[0]);
-            int m = Integer.parseInt(input[1]);
-            
+        while (t-- > 0) {
             String S1 = read.readLine().trim();
             String S2 = read.readLine().trim();
 
             Solution ob = new Solution();
-            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+            System.out.println(ob.longestCommonSubstr(S1, S2));
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution{
-    int longestCommonSubstr(String S1, String S2, int n, int m){
+class Solution {
+    public int longestCommonSubstr(String str1, String str2) {
         // code here
+        int n = str1.length();
+        int m = str2.length();
         int dp [][] = new int[n+1][m+1];
         int ans =0;
         for(int i =0;i<n+1;i++){
@@ -43,7 +38,7 @@ class Solution{
         }
         for(int i = 1;i<n+1;i++){
             for(int j =1;j<m+1;j++){
-                if(S1.charAt(i-1)==S2.charAt(j-1)){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1]+1;
                     ans =Math.max(ans,dp[i][j]);
                 }
