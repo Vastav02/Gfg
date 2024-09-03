426db4c42ef6fa26cde32265b7ef5eb4f594edb6
Vastav02 <95334618+Vastav02@users.noreply.github.com>
2024-09-03 19:32:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.java
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.java
@@ -27,22 +27,32 @@ class Solution
     public int minOperations(String str1, String str2) 
     { 
         // Your code goes here
-        int n = str1.length();
-        int m = str2.length();
-        
-        int[][] dp = new int[n+1][m+1];
-       
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+1;
+       int n =str1.length();
+        int m =str2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j =0;j<m+1;j++){
+                if(i==0){
+                    dp[i][j]=j;
                 }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if(j==0){
+                    dp[i][j]=i;
                 }
             }
         }
-        int ans = dp[n][m];
-        return (n-ans)+(m-ans);
-    } 
+        for(int i=1;i<n+1;i++){
+            for(int j =1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int add = dp[i][j-1]+1;
+                    int delete = dp[i-1][j]+1;
+                    dp[i][j] = Math.min(add, delete);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    
 }
